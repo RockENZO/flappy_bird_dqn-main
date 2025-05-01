1b2b0fe9b4f4492ef8f98655d7c83b711912eb8b
RockENZO <gewang2004@gmail.com>
2025-05-02 00:42:45 +0930

    level 3 update

--- my_agent.py
+++ my_agent.py
@@ -207,7 +207,7 @@ class MyAgent:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--level', type=int, default=2)
+    parser.add_argument('--level', type=int, default=3)
 
     args = parser.parse_args()
 
